2856fb78279dd68a18006f64c65132beff421e63
alexpodjelskiy <podjelskiy@gmail.com>
2022-11-08 14:39:44 +0300

    feat: add copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+let dirPath = path.join(__dirname, 'files-copy')
+
+fs.rm(dirPath, { recursive: true, force: true }, () => {
+  fs.mkdir(dirPath, { recursive: true }, () => {
+    fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (error, files) => {
+      if (error) {
+        console.log(error.message);
+      } else {
+        for (let file of files) {
+          let files = path.join(__dirname, 'files', file.name);
+          let filesCopy = path.join(__dirname, 'files-copy', file.name);
+          fs.copyFile(files, filesCopy, error => {
+            if (error) console.log(error.message);
+          });
+        };
+      }
+    });
+  });
+});
